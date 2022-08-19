d2a629e68ebdc7ee93e3f4bbe02d7a3fb04cff72
linarium <linarsaif@gmail.com>
2022-08-19 20:09:56 +0500

    first version of GoTrip website

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+// Carousel //
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        startPosition: 1,
+        items: 3,
+        responsive: {
+            540: {
+                items: 3,
+                startPosition: 1,
+            },
+            1200: {
+                items: 3,
+                margin: 30,
+            },
+        },
+});
+$('.slider__btn--prev').click(function() {
+
+    owl.trigger('prev.owl.carousel');
+})
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+
+// Menu ICon //
+
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+
+navBtn.onclick = function() {
+    nav.classList.toggle('nav--mobile')
+   menuIcon.classList.toggle('menu-icon--active');
+}
+
